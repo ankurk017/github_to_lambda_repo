997be4cc3b70cc68bdc98f6a80fe35df654f5032
ankurk017 <ankurk017@gmail.com>
2022-12-28 15:44:18 -0600

    Updated numpy

--- configuration/lambda_function.py
+++ configuration/lambda_function.py
@@ -1,4 +1,7 @@
-
+import numpy as np
 
 def lambda_handler(event, context):
-    print('Ankur Kumar | Done x1.1.1')
+    output = np.random.rand(3,2)
+    print(output)
+
+    print('Ankur Kumar | Done x2')
