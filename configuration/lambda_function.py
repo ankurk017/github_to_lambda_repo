764be4e9c7c01d79e4967211d976b4949d0d0005
ankurk017 <ankurk017@gmail.com>
2022-12-30 20:12:47 -0600

    Updated requirements.txt

--- configuration/lambda_function.py
+++ configuration/lambda_function.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pandas as pd
+
 def lambda_handler(event, context):
-#    output = np.random.rand(3,2)
-#    print(output)
+    output = np.random.rand(3,2)
+    print(output)
 
     print('Ankur Kumar | Done 20221220')
